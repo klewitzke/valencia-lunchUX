2397f65d27b3a87c5a23157830977a37434ec459
klewitzke <klewitzke@knights.ucf.edu>
2016-03-01 03:08:01 -0500

    remove unused files

--- dbconnect.php
+++ dbconnect.php
@@ -1,8 +1,8 @@
 <?php
-$serverName = 'LEWITZKE\sqlexpress'; //serverName\instanceName
-$username = 'dbconnect';
-$password = 'dbconnect';
-$dbname = 'ResSys';
-$pdo = new PDO("sqlsrv:Server=".$serverName.";Database=".$dbname, $username, $password);
+$serverName = 'mysql13.000webhost.com';
+$username = 'a5455109_LunchUX';
+$password = 'LunchUX';
+$dbname = 'a5455109_LunchUX';
+$pdo = new PDO("mysql:host=".$serverName.";dbname=".$dbname, $username, $password);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
